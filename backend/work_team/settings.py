46a550b6735848e4d1e403dedc453ffa569ddc23
TWiSTeD <151539904+TWiSTeD707@users.noreply.github.com>
2025-02-17 19:36:41 +0400

    Add files via upload

--- backend/work_team/settings.py
+++ backend/work_team/settings.py
@@ -1,1 +1,22 @@
+INSTALLED_APPS = [
+    'rest_framework',
+    'rest_framework_simplejwt',
+    'core',
+]
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+}
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'workteam',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'db',
+        'PORT': '5432',
+    }
+}
